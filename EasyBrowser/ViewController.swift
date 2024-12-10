f8060573166ed6b7a538b006a7483f62d813d4bb
aydinnkaya <aydinnkaya1@gmail.com>
2024-12-10 18:22:39 +0300

    bottom bar

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView : WKWebView!
+    var progressView : UIProgressView!
     
     override func loadView() {
         
@@ -22,9 +23,23 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         
+        super.viewDidLoad()
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        super.viewDidLoad()
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        
+        
         let url = URL(string: "https://www.hackingwithswift.com/100")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
@@ -53,7 +68,6 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        
         title = webView.title
     }
                      
